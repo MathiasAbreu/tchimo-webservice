84fae4be5df3d412d06e574b03aaacef658ca5ca
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2020-10-07 10:48:23 -0300

    Primeiro commit do projeto. Template básico.

---
+++ src/main/java/br/com/ufcg/back/LetsWorkTogetherApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufcg.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsWorkTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsWorkTogetherApplication.class, args);
+	}
+
+}
