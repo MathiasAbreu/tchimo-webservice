41e84872ba8c6c7cd910ba9a9baf6381d28a7905
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2020-10-09 20:06:43 -0300

    Habilitação do CORS.

--- src/main/java/br/com/ufcg/back/LetsWorkTogetherApplication.java
+++ src/main/java/br/com/ufcg/back/LetsWorkTogetherApplication.java
@@ -2,11 +2,29 @@ package br.com.ufcg.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class LetsWorkTogetherApplication {
 
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(LetsWorkTogetherApplication.class, args);
 	}
 
